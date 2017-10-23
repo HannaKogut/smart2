c7d10ca3fab5546998852c95aed00b53da0e1b75
HannaKogut <hanna.pol94@gmail.com>
2017-10-23 20:59:27 +0300

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {TimelineLite, TweenLite} from 'gsap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'app';
+
+	showDescription() {
+
+		let logo = document.getElementById("logo");
+TweenLite.to(logo, 2, {width:"200px", height:"550px"})
+	}
 }
