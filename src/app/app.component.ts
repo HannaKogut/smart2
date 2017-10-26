96e66c20d9204a77b916dab8eec3b8a4996a351a
HannaKogut <hanna.pol94@gmail.com>
2017-10-26 18:03:32 +0300

    logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {TimelineLite, TweenLite} from 'gsap';
+import { TimelineLite, TweenLite, Elastic} from 'gsap';
+import { MainComponent } from './main/main.component';
+
 
 @Component({
   selector: 'app-root',
@@ -9,11 +11,14 @@ import {TimelineLite, TweenLite} from 'gsap';
 
 
 
+
 export class AppComponent {
-
-	showDescription() {
-
-		let logo = document.getElementById("logo");
-TweenLite.to(logo, 2, {width:"200px", height:"550px"})
-	}
+showDescription() {
+    let logo = document.getElementById('logo');
+    TweenLite.to(logo, 0.3, { opacity: 0.1 });
+    let mainDescription = document.getElementById('mainDescription');
+    TweenLite.to(mainDescription, 1, {opacity: 1, ease: Elastic.easeOut})
+    let dialogSVG = document.getElementsByClassName('dialogSVG');
+    TweenLite.fromTo(dialogSVG, 1, {y: -150,}, {y: 0, opacity: 1, ease: Elastic.easeOut})
+  }
 }
